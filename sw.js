bf42ccb77326f82816c6c19a9e9405ea518a7707
colinl2-ops <colinl2@yahoo.com>
2026-07-11 22:02:45 +1000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "med-helper-cache-v20260711215724";
-const ASSETS = ["./", "./index.html", "./styles.css?v=20260711-215724", "./app.js?v=20260711-215724", "./manifest.webmanifest", "./icons/icon-192.png", "./icons/icon-512.png"];
+const CACHE_NAME = "med-helper-cache-v20260711220152";
+const ASSETS = ["./", "./index.html", "./styles.css?v=20260711-220152", "./app.js?v=20260711-220152", "./manifest.webmanifest", "./icons/icon-192.png", "./icons/icon-512.png"];
 
 self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
